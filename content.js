730216d3650965bf78ec75d54275b6a2341cd650
sarsntony <sarsntony@gmail.com>
2021-04-10 23:46:40 -0700

    Basic styling

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const tabContainer = document.getElementsByClassName('sncTabSetContainer')[0];
+const tabContainer = document.getElementsByClassName('sncTabSetList')[0];
 
 const betterVanguardTabs = document.createElement('ul');
 betterVanguardTabs.setAttribute("class", "betterVanguard")
@@ -8,14 +8,9 @@ graphTab.setAttribute("class", "betterVanguardUnselectedTab")
 
 const graphAnchor = document.createElement("a");
 graphAnchor.setAttribute("id", "betterVanguardGraphTab");
-graphAnchor.setAttribute("href", "");
-graphAnchor.setAttribute("role", "tab");
-graphAnchor.setAttribute("tabindex", "-1");
-graphAnchor.setAttribute("aria-selected", "false");
 graphAnchor.setAttribute("aria-controls", "betterVanguardGraphTab");
-graphAnchor.setAttribute("class", "sncTabSetUnselectedLink");
 graphAnchor.text = "Graph"
 
 graphTab.append(graphAnchor);
 betterVanguardTabs.append(graphTab);
-tabContainer.after(betterVanguardTabs);
+tabContainer.after(betterVanguardTabs); 
